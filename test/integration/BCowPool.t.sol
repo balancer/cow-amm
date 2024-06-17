ec6717506bb185e3a0c89ab50f09506a4065941e
Weißer Hase <wei3erhase@defi.sucks>
2024-06-17 14:18:21 +0200

    feat: adding inverse path on integration test (#96)
    * test: small improvements, add cowFactory
    
    * refactor: simplifying setUp
    
    * refactor: casting pool to IBPool on tests
    
    * fix: comment

--- test/integration/BCowPool.t.sol
+++ test/integration/BCowPool.t.sol
@@ -10,13 +10,13 @@ import {GPv2Trade} from '@cowprotocol/libraries/GPv2Trade.sol';
 import {GPv2Signing} from '@cowprotocol/mixins/GPv2Signing.sol';
 import {BCoWConst} from 'contracts/BCoWConst.sol';
 import {BCoWFactory} from 'contracts/BCoWFactory.sol';
+
 import {IBCoWPool} from 'interfaces/IBCoWPool.sol';
+import {IBFactory} from 'interfaces/IBFactory.sol';
 import {ISettlement} from 'interfaces/ISettlement.sol';
 
 contract BCowPoolIntegrationTest is PoolSwapIntegrationTest, BCoWConst {
   using GPv2Order for GPv2Order.Data;
-
-  BCoWFactory public cowfactory;
 
   address public solver = address(0xa5559C2E1302c5Ce82582A6b1E4Aec562C2FbCf4);
 
@@ -25,37 +25,20 @@ contract BCowPoolIntegrationTest is PoolSwapIntegrationTest, BCoWConst {
   bytes32 public constant APP_DATA = bytes32('exampleIntegrationAppData');
 
   function setUp() public override {
-    vm.createSelectFork('mainnet', 20_012_063);
+    super.setUp();
 
-    deal(address(dai), lp, HUNDRED_UNITS);
-    deal(address(weth), lp, HUNDRED_UNITS);
-
-    deal(address(dai), swapper.addr, ONE_UNIT);
-
-    cowfactory = new BCoWFactory(address(settlement));
-
-    vm.startPrank(lp);
-    pool = address(cowfactory.newBPool());
-
-    dai.approve(pool, type(uint256).max);
-    weth.approve(pool, type(uint256).max);
-    IBCoWPool(pool).bind(address(dai), ONE_UNIT, 2e18); // 20% weight
-    IBCoWPool(pool).bind(address(weth), ONE_UNIT, 8e18); // 80% weight
-    // finalize
-    IBCoWPool(pool).finalize();
     // enable trading
-    IBCoWPool(pool).enableTrading(APP_DATA);
+    IBCoWPool(address(pool)).enableTrading(APP_DATA);
+  }
 
-    // clean dai and weth from the settlement
-    vm.startPrank(address(settlement));
-    dai.transfer(address(0), dai.balanceOf(address(settlement)));
-    weth.transfer(address(0), weth.balanceOf(address(settlement)));
+  function _deployFactory() internal override returns (IBFactory) {
+    return new BCoWFactory(address(settlement));
   }
 
   function _makeSwap() internal override {
     uint32 latestValidTimestamp = uint32(block.timestamp) + MAX_ORDER_DURATION - 1;
 
-    // swapper approves weth to vaultRelayer
+    // swapper approves dai to vaultRelayer
     vm.startPrank(swapper.addr);
     dai.approve(settlement.vaultRelayer(), type(uint256).max);
 
@@ -144,16 +127,122 @@ contract BCowPoolIntegrationTest is PoolSwapIntegrationTest, BCoWConst {
       [new GPv2Interaction.Data[](1), new GPv2Interaction.Data[](0), new GPv2Interaction.Data[](0)];
 
     interactions[0][0] = GPv2Interaction.Data({
-      target: pool,
+      target: address(pool),
       value: 0,
       callData: abi.encodeWithSelector(
-        IBCoWPool.commit.selector, poolOrder.hash(IBCoWPool(pool).SOLUTION_SETTLER_DOMAIN_SEPARATOR())
+        IBCoWPool.commit.selector, poolOrder.hash(IBCoWPool(address(pool)).SOLUTION_SETTLER_DOMAIN_SEPARATOR())
       )
     });
 
     // finally, settle
     vm.startPrank(solver);
-    snapStart('settle');
+    snapStart('settlementCoWSwap');
+    settlement.settle(tokens, clearingPrices, trades, interactions);
+    snapEnd();
+  }
+
+  function _makeSwapInverse() internal override {
+    uint32 latestValidTimestamp = uint32(block.timestamp) + MAX_ORDER_DURATION - 1;
+
+    // swapper approves weth to vaultRelayer
+    vm.startPrank(swapperInverse.addr);
+    weth.approve(settlement.vaultRelayer(), type(uint256).max);
+
+    // swapper creates the order
+    GPv2Order.Data memory swapperOrder = GPv2Order.Data({
+      sellToken: weth,
+      buyToken: dai,
+      receiver: GPv2Order.RECEIVER_SAME_AS_OWNER,
+      sellAmount: WETH_AMOUNT_INVERSE,
+      buyAmount: DAI_AMOUNT_INVERSE,
+      validTo: latestValidTimestamp,
+      appData: APP_DATA,
+      feeAmount: 0,
+      kind: GPv2Order.KIND_BUY,
+      partiallyFillable: false,
+      buyTokenBalance: GPv2Order.BALANCE_ERC20,
+      sellTokenBalance: GPv2Order.BALANCE_ERC20
+    });
+
+    // swapper signs the order
+    (uint8 v, bytes32 r, bytes32 s) =
+      vm.sign(swapperInverse.privateKey, GPv2Order.hash(swapperOrder, settlement.domainSeparator()));
+    bytes memory swapperSig = abi.encodePacked(r, s, v);
+
+    // order for bPool is generated
+    GPv2Order.Data memory poolOrder = GPv2Order.Data({
+      sellToken: dai,
+      buyToken: weth,
+      receiver: GPv2Order.RECEIVER_SAME_AS_OWNER,
+      sellAmount: DAI_AMOUNT_INVERSE,
+      buyAmount: WETH_AMOUNT_INVERSE,
+      validTo: latestValidTimestamp,
+      appData: APP_DATA,
+      feeAmount: 0,
+      kind: GPv2Order.KIND_SELL,
+      partiallyFillable: true,
+      sellTokenBalance: GPv2Order.BALANCE_ERC20,
+      buyTokenBalance: GPv2Order.BALANCE_ERC20
+    });
+    bytes memory poolSig = abi.encode(poolOrder);
+
+    // solver prepares for call settle()
+    IERC20[] memory tokens = new IERC20[](2);
+    tokens[0] = IERC20(dai);
+    tokens[1] = IERC20(weth);
+
+    uint256[] memory clearingPrices = new uint256[](2);
+    // TODO: we can use more accurate clearing prices here
+    clearingPrices[0] = WETH_AMOUNT_INVERSE;
+    clearingPrices[1] = DAI_AMOUNT_INVERSE;
+
+    GPv2Trade.Data[] memory trades = new GPv2Trade.Data[](2);
+
+    // pool's trade
+    trades[0] = GPv2Trade.Data({
+      sellTokenIndex: 0,
+      buyTokenIndex: 1,
+      receiver: poolOrder.receiver,
+      sellAmount: poolOrder.sellAmount,
+      buyAmount: poolOrder.buyAmount,
+      validTo: poolOrder.validTo,
+      appData: poolOrder.appData,
+      feeAmount: poolOrder.feeAmount,
+      flags: GPv2TradeEncoder.encodeFlags(poolOrder, GPv2Signing.Scheme.Eip1271),
+      executedAmount: poolOrder.sellAmount,
+      signature: abi.encodePacked(pool, poolSig)
+    });
+
+    // swapper's trade
+    trades[1] = GPv2Trade.Data({
+      sellTokenIndex: 1,
+      buyTokenIndex: 0,
+      receiver: swapperOrder.receiver,
+      sellAmount: swapperOrder.sellAmount,
+      buyAmount: swapperOrder.buyAmount,
+      validTo: swapperOrder.validTo,
+      appData: swapperOrder.appData,
+      feeAmount: swapperOrder.feeAmount,
+      flags: GPv2TradeEncoder.encodeFlags(swapperOrder, GPv2Signing.Scheme.Eip712),
+      executedAmount: swapperOrder.sellAmount,
+      signature: swapperSig
+    });
+
+    // in the first interactions, save the commitment
+    GPv2Interaction.Data[][3] memory interactions =
+      [new GPv2Interaction.Data[](1), new GPv2Interaction.Data[](0), new GPv2Interaction.Data[](0)];
+
+    interactions[0][0] = GPv2Interaction.Data({
+      target: address(pool),
+      value: 0,
+      callData: abi.encodeWithSelector(
+        IBCoWPool.commit.selector, poolOrder.hash(IBCoWPool(address(pool)).SOLUTION_SETTLER_DOMAIN_SEPARATOR())
+      )
+    });
+
+    // finally, settle
+    vm.startPrank(solver);
+    snapStart('settlementCoWSwapInverse');
     settlement.settle(tokens, clearingPrices, trades, interactions);
     snapEnd();
   }
--- test/integration/PoolSwap.t.sol
+++ test/integration/PoolSwap.t.sol
@@ -9,7 +9,7 @@ import {IBFactory} from 'interfaces/IBFactory.sol';
 import {IBPool} from 'interfaces/IBPool.sol';
 
 abstract contract PoolSwapIntegrationTest is Test, GasSnapshot {
-  address public pool;
+  IBPool public pool;
   IBFactory public factory;
 
   IERC20 public dai = IERC20(0x6B175474E89094C44Da98b954EedeAC495271d0F);
@@ -18,59 +18,100 @@ abstract contract PoolSwapIntegrationTest is Test, GasSnapshot {
   address public lp = makeAddr('lp');
 
   Vm.Wallet swapper = vm.createWallet('swapper');
+  Vm.Wallet swapperInverse = vm.createWallet('swapperInverse');
 
   uint256 public constant HUNDRED_UNITS = 100 ether;
   uint256 public constant ONE_UNIT = 1 ether;
+
   // NOTE: hardcoded from test result
   uint256 public constant WETH_AMOUNT = 0.096397921069149814e18;
   uint256 public constant DAI_AMOUNT = 0.5e18;
 
+  uint256 public constant WETH_AMOUNT_INVERSE = 0.1e18;
+  // NOTE: hardcoded from test result
+  uint256 public constant DAI_AMOUNT_INVERSE = 0.316986296266343639e18;
+
   function setUp() public virtual {
     vm.createSelectFork('mainnet', 20_012_063);
 
-    factory = new BFactory();
+    factory = _deployFactory();
 
     deal(address(dai), lp, HUNDRED_UNITS);
     deal(address(weth), lp, HUNDRED_UNITS);
 
     deal(address(dai), swapper.addr, ONE_UNIT);
+    deal(address(weth), swapperInverse.addr, ONE_UNIT);
 
     vm.startPrank(lp);
-    pool = address(factory.newBPool());
+    pool = factory.newBPool();
 
-    dai.approve(pool, type(uint256).max);
-    weth.approve(pool, type(uint256).max);
-    IBPool(pool).bind(address(dai), ONE_UNIT, 2e18); // 20% weight
-    IBPool(pool).bind(address(weth), ONE_UNIT, 8e18); // 80% weight
+    dai.approve(address(pool), type(uint256).max);
+    weth.approve(address(pool), type(uint256).max);
+    pool.bind(address(dai), ONE_UNIT, 2e18); // 20% weight
+    pool.bind(address(weth), ONE_UNIT, 8e18); // 80% weight
     // finalize
-    IBPool(pool).finalize();
+    pool.finalize();
   }
 
   function testSimpleSwap() public {
     _makeSwap();
-    assertEq(dai.balanceOf(swapper.addr), DAI_AMOUNT);
+    assertEq(dai.balanceOf(swapper.addr), ONE_UNIT - DAI_AMOUNT);
     assertEq(weth.balanceOf(swapper.addr), WETH_AMOUNT);
 
     vm.startPrank(lp);
 
-    uint256 lpBalance = IBPool(pool).balanceOf(lp);
-    IBPool(pool).exitPool(lpBalance, new uint256[](2));
+    uint256 lpBalance = pool.balanceOf(lp);
+    pool.exitPool(lpBalance, new uint256[](2));
 
     assertEq(dai.balanceOf(lp), HUNDRED_UNITS + DAI_AMOUNT); // initial 100 + 0.5 dai
     assertEq(weth.balanceOf(lp), HUNDRED_UNITS - WETH_AMOUNT); // initial 100 - ~0.09 weth
   }
 
+  function testSimpleSwapInverse() public {
+    _makeSwapInverse();
+    assertEq(dai.balanceOf(swapperInverse.addr), DAI_AMOUNT_INVERSE);
+    assertEq(weth.balanceOf(swapperInverse.addr), ONE_UNIT - WETH_AMOUNT_INVERSE);
+
+    vm.startPrank(lp);
+
+    uint256 lpBalance = pool.balanceOf(address(lp));
+    pool.exitPool(lpBalance, new uint256[](2));
+
+    assertEq(dai.balanceOf(address(lp)), HUNDRED_UNITS - DAI_AMOUNT_INVERSE); // initial 100 - ~0.5 dai
+    assertEq(weth.balanceOf(address(lp)), HUNDRED_UNITS + WETH_AMOUNT_INVERSE); // initial 100 + 0.1 tokenB
+  }
+
+  function _deployFactory() internal virtual returns (IBFactory);
+
   function _makeSwap() internal virtual;
+
+  function _makeSwapInverse() internal virtual;
 }
 
 contract DirectPoolSwapIntegrationTest is PoolSwapIntegrationTest {
+  function _deployFactory() internal override returns (IBFactory) {
+    return new BFactory();
+  }
+
   function _makeSwap() internal override {
     vm.startPrank(swapper.addr);
-    dai.approve(pool, type(uint256).max);
+    dai.approve(address(pool), type(uint256).max);
 
     // swap 0.5 dai for weth
     snapStart('swapExactAmountIn');
-    IBPool(pool).swapExactAmountIn(address(dai), DAI_AMOUNT, address(weth), 0, type(uint256).max);
+    pool.swapExactAmountIn(address(dai), DAI_AMOUNT, address(weth), 0, type(uint256).max);
+    snapEnd();
+
+    vm.stopPrank();
+  }
+
+  function _makeSwapInverse() internal override {
+    vm.startPrank(swapperInverse.addr);
+    weth.approve(address(pool), type(uint256).max);
+
+    // swap 0.1 weth for dai
+    snapStart('swapExactAmountInInverse');
+    pool.swapExactAmountIn(address(weth), WETH_AMOUNT_INVERSE, address(dai), 0, type(uint256).max);
     snapEnd();
 
     vm.stopPrank();
