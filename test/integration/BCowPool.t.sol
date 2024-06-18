a51072bcc50aa20b3cc9ea1a7670c026d3edaeb3
Austrian <114922365+0xAustrian@users.noreply.github.com>
2024-06-18 11:54:26 -0300

    feat: vendor GPv2TradeEncoder (#105)
    * feat: vendor GPv2TradeEncoder
    
    * feat: update composable-cow dependency commit
    
    * feat: add another remapping to reduce number of changes
    
    * feat: rename composable-cow remapping

--- test/integration/BCowPool.t.sol
+++ test/integration/BCowPool.t.sol
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.25;
 
-import {GPv2TradeEncoder} from './GPv2TradeEncoder.sol';
 import {PoolSwapIntegrationTest} from './PoolSwap.t.sol';
+import {GPv2TradeEncoder} from '@composable-cow/test/vendored/GPv2TradeEncoder.sol';
 import {IERC20} from '@cowprotocol/interfaces/IERC20.sol';
 import {GPv2Interaction} from '@cowprotocol/libraries/GPv2Interaction.sol';
 import {GPv2Order} from '@cowprotocol/libraries/GPv2Order.sol';
