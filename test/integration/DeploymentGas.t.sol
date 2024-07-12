d00b1eb3e192f9fda0a09ff6ed2416c9932583cd
Weißer Hase <wei3erhase@defi.sucks>
2024-07-12 09:40:25 +0200

    feat: adding bCoW deployment snapshots and updating snaps (#151)
    * feat: adding bCoW deployment snapshots and updating snaps
    
    * fix: re-updated snaps
    
    * fix: manually updating BFactory snap to match

--- test/integration/DeploymentGas.t.sol
+++ test/integration/DeploymentGas.t.sol
@@ -1,27 +1,48 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.25;
 
+import {BCoWFactory} from 'contracts/BCoWFactory.sol';
 import {BFactory} from 'contracts/BFactory.sol';
 
 import {GasSnapshot} from 'forge-gas-snapshot/GasSnapshot.sol';
 import {Test} from 'forge-std/Test.sol';
 
+contract MockSolutionSettler {
+  address public vaultRelayer;
+  bytes32 public domainSeparator;
+}
+
 contract DeploymentIntegrationGasTest is Test, GasSnapshot {
-  BFactory public factory;
+  BFactory public bFactory;
+  BCoWFactory public bCowFactory;
+  address solutionSettler;
+  address deployer = makeAddr('deployer');
 
   function setUp() public {
-    factory = new BFactory();
+    vm.startPrank(deployer);
+    bFactory = new BFactory();
+
+    solutionSettler = address(new MockSolutionSettler());
+    bCowFactory = new BCoWFactory(solutionSettler, bytes32('appData'));
   }
 
   function testFactoryDeployment() public {
     snapStart('newBFactory');
     new BFactory();
     snapEnd();
+
+    snapStart('newBCoWFactory');
+    new BCoWFactory(solutionSettler, bytes32('appData'));
+    snapEnd();
   }
 
-  function testDeployment() public {
+  function testPoolDeployment() public {
     snapStart('newBPool');
-    factory.newBPool();
+    bFactory.newBPool();
+    snapEnd();
+
+    snapStart('newBCoWPool');
+    bCowFactory.newBPool();
     snapEnd();
   }
 }
